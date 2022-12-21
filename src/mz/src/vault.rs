29e73200d89a7e23877bbb8819ccf883e580bd4f
Seth Wiesman <sjwiesman@gmail.com>
2022-12-21 13:43:54 -0600

    mz: add apple keychain vault type

--- src/mz/src/vault.rs
+++ src/mz/src/vault.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::Parser;
 use serde::{de::Visitor, Deserialize, Serialize};
 
@@ -21,37 +21,76 @@ use crate::configuration::FronteggAPIToken;
 
 const INLINE_PREFIX: &str = "mzp_";
 
+#[cfg(target_os = "macos")]
+const APPLE_KEYCHAIN: &str = "apple_keychain";
+
 /// A vault stores sensitive information and provides
 /// back a token than can be used for latter retrieval.
 /// If no specific vault is selected, values are inlined
 /// directly in the token.
 #[derive(Parser, Debug)]
 #[clap(group = clap::ArgGroup::new("vault").multiple(false))]
-pub struct Vault {}
+pub struct Vault {
+    /// Store the app password in the local
+    /// apple keychain.
+    #[cfg(target_os = "macos")]
+    #[clap(long, short, group = "vault")]
+    apple: bool,
+}
 
 impl Vault {
     pub async fn store(
         &self,
-        _profile: &str,
-        _email: &str,
+        profile: &str,
+        email: &str,
         api_token: FronteggAPIToken,
     ) -> Result<Token> {
         let password = api_token.to_string();
+
+        #[cfg(target_os = "macos")]
+        if self.apple {
+            use security_framework::passwords;
+
+            let account = apple_account_id(profile, email);
+            passwords::set_generic_password("Materialize CLI", &account, password.as_bytes())
+                .context("failed to store password in apple keychain")?;
+
+            return Ok(Token::AppleKeyChain);
+        }
 
         Ok(Token::Inline(password))
     }
 }
 
 pub enum Token {
+    #[cfg(target_os = "macos")]
+    AppleKeyChain,
     Inline(String),
 }
 
 impl Token {
-    pub async fn retrieve(&self, _profile: &str, _email: &str) -> Result<String> {
+    pub async fn retrieve(&self, profile: &str, email: &str) -> Result<String> {
         match self {
+            #[cfg(target_os = "macos")]
+            Token::AppleKeyChain => {
+                use security_framework::passwords;
+
+                let account = apple_account_id(profile, email);
+                let bytes = passwords::get_generic_password("Materialize CLI", &account)
+                    .context("failed to retrieve password from apple keychain")?;
+
+                let app_password = String::from_utf8(bytes)
+                    .context("failed decode password from apple keychain")?;
+
+                Ok(app_password)
+            }
             Token::Inline(app_password) => Ok(app_password.to_string()),
         }
     }
+}
+
+fn apple_account_id(profile: &str, email: &str) -> String {
+    format!("{profile}:{email}")
 }
 
 /// Custom debug implementation
@@ -59,6 +98,8 @@ impl Token {
 impl std::fmt::Debug for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            #[cfg(target_os = "macos")]
+            Self::AppleKeyChain => write!(f, "AppleKeyChain"),
             Self::Inline(_) => write!(f, "Inline(_)"),
         }
     }
@@ -70,6 +111,8 @@ impl Serialize for Token {
         S: serde::Serializer,
     {
         match self {
+            #[cfg(target_os = "macos")]
+            Token::AppleKeyChain => serializer.serialize_str(APPLE_KEYCHAIN),
             Token::Inline(app_password) => serializer.serialize_str(app_password),
         }
     }
@@ -84,6 +127,11 @@ impl<'de> Visitor<'de> for TokenVisitor {
     where
         E: serde::de::Error,
     {
+        #[cfg(target_os = "macos")]
+        if v == APPLE_KEYCHAIN {
+            return Ok(Token::AppleKeyChain);
+        }
+
         if v.starts_with(INLINE_PREFIX) {
             return Ok(Token::Inline(v.to_string()));
         }
